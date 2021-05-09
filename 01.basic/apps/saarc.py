7b327c678c0cb14567a432f2752428c2316d7e89
Arka Barua <arka@pop-os.localdomain>
2021-05-09 13:01:15 +0600

    I have updated some code

--- 01.basic/apps/saarc.py
+++ 01.basic/apps/saarc.py
@@ -1,11 +1,11 @@
-#i have listed the countries those are under saarc 
+# i have listed the countries those are under saarc
 
-saarc = ['Bangladesh', 'Afghanistan', 'Bhutan','Nepal', 'India', 'Pakistan', 'Sri Lanka',
-'bangladesh', 'afghanistan', 'bhutan','nepal', 'india', 'pakistan', 'sri lanka', 'sri lanka']
+saarc = ['Bangladesh', 'Afghanistan', 'Bhutan', 'Nepal', 'India', 'Pakistan', 'Sri Lanka', 'Maldives',
+         'bangladesh', 'afghanistan', 'bhutan', 'nepal', 'india', 'pakistan', 'sri lanka', 'sri lanka', 'maldives']
 
 X = input('Enter the name of the country:')
 
 if X in saarc:
-    print( X ,"is under the saarc")
+    print(X, "is under the saarc")
 else:
-    print( X ,"is not under the saarc, or it is not an country")
+    print(X, "is not under the saarc, or it is not an country")
--- 02.basic/id.py
+++ 02.basic/id.py
@@ -5,21 +5,21 @@ y = 10
 
 z = 25
 
-print( x is y)
-print( x is not z)
+print( x != y) # print( x is y)
+print( x is not z)# print ( x != y)
 
 list1 = [1, 2, 3, 4]
 list2 = [1, 2, 3, 4]
 list3 = [1, 2, 3]
 
-print(list1 is list2)
-print(list1 is not list2)
-print(list1 == list2)
-print(list == list3)
+print("1 ",list1 is list2)
+print("2 ",list1 is not list2)
+print("3 ",list1 == list2)
+print("4 ",list == list3)
 
 #membership operators
 name = 'Arka Barua'
 
-print('A' in name)
-print('H' in name)
-print('O' not in name)
+print("5",'A' in name)
+print("6",'H' in name)
+print("7",'O' not in name)
